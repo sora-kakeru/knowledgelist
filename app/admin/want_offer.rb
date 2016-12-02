205376269450d225ec75e72c33c0ada68963b176
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:50:11 +0000

    Whitelist columns in WantOffer for ActiveAdmin

--- app/admin/want_offer.rb
+++ app/admin/want_offer.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register WantOffer do
 
+ permit_params :offer_id, :want_id, :match
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
