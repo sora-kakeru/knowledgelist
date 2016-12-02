9a37be5012aca5f752e14da81bdc5b757d0a213b
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:12 +0000

    Validate uniqueness of want_offers.offer_id

--- app/models/want_offer.rb
+++ app/models/want_offer.rb
@@ -5,6 +5,8 @@ class WantOffer < ApplicationRecord
 
   # Validations
 
+  validates :offer_id, :uniqueness => { :scope => [:want_id] }
+
   validates :offer_id, :presence => true
 
   validates :want_id, :uniqueness => { :scope => [:offer_id] }
