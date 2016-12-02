d11e4003962621813805b8b9afcea221f2f03f11
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:57 +0000

    Whitelist columns in CounselorLike for ActiveAdmin

--- app/admin/counselor_like.rb
+++ app/admin/counselor_like.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CounselorLike do
 
+ permit_params :want_id, :counselor_id, :like
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
