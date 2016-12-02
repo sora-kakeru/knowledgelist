a46b51627e382133f405a59e28f56cc9fa0a7973
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:50:11 +0000

    Only want user can modify want

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,4 +1,14 @@
 class WantsController < ApplicationController
+  before_action :current_user_must_be_want_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_want_user
+    want = Want.find(params[:id])
+
+    unless current_user == want.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @wants = Want.all
 
