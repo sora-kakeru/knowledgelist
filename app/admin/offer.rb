625b595bde3385bdf9d74e8741b0f64921613e1e
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:46 +0000

    Whitelist columns in Offer for ActiveAdmin

--- app/admin/offer.rb
+++ app/admin/offer.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Offer do
 
+ permit_params :counselor_id, :offers
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
