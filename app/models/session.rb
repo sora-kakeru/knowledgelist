f8134db8ba8695d06457e4144eca6905be0be856
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:12 +0000

    Validate numericality of sessions.user_stars

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,6 +5,8 @@ class Session < ApplicationRecord
 
   # Validations
 
+  validates :user_stars, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1 }
+
   validates :want_offer_id, :uniqueness => true
 
   validates :want_offer_id, :presence => true
