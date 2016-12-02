404a8a68f7d21aa4a1fc01704912810af883e975
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:12 +0000

    Validate uniqueness of counselor_likes.counselor_id

--- app/models/counselor_like.rb
+++ app/models/counselor_like.rb
@@ -5,6 +5,8 @@ class CounselorLike < ApplicationRecord
 
   # Validations
 
+  validates :counselor_id, :uniqueness => { :scope => [:want_id] }
+
   validates :counselor_id, :presence => true
 
   validates :want_id, :presence => true
