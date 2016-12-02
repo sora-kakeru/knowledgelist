f80ed55cbd1918550c3a05938fd0fe0ccc2e794a
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:50:02 +0000

    Whitelist columns in OfferTag for ActiveAdmin

--- app/admin/offer_tag.rb
+++ app/admin/offer_tag.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register OfferTag do
 
+ permit_params :offer_id, :tag_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
