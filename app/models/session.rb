c16b4ae65ffa9400fe6434f4fb964a1a6e8b0763
Raghu Betina <raghu@firstdraft.com>
2016-12-02 02:49:12 +0000

    Validate presence of sessions.user_stars

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,6 +5,8 @@ class Session < ApplicationRecord
 
   # Validations
 
+  validates :user_stars, :presence => true
+
   validates :user_stars, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1 }
 
   validates :want_offer_id, :uniqueness => true
